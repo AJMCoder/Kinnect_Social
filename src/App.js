d6962bcf079b052a7ae95b1b775e41bc95de808b
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-22 17:27:18 +0000

    Add the react router library and configure the navlinks to the different pages for the website.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavBar from "./components/NavBar";
 import styles from './App.module.css';
+import { Container } from 'react-bootstrap';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/register" render={() => <h1>Register</h1>} />
+          <Route render={() => <h1>404 Not Found</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,22 @@
 import React from "react";
 import styles from '../styles/NavBar.module.css';
 import { Navbar, Nav, Container, Form, FormControl, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <Container>
             <div>
                 <Navbar className={styles.NavBar} bg="dark" expand='md' fixed="top" variant="dark">
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <NavLink to="/">
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    </NavLink>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-                            <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
-                            <Nav.Link><i className="fas fa-user-plus"></i>Register</Nav.Link>
+                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/"><i className="fas fa-home"></i>Home</NavLink>
+                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i className="fas fa-sign-in-alt"></i>Sign In</NavLink>
+                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/register"><i className="fas fa-user-plus"></i>Register</NavLink>
                         </Nav>
                         <Form inline>
                             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
