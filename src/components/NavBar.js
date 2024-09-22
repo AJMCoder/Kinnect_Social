519270072b62460daa7d392efe246606dc412e6d
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-22 19:16:38 +0000

    Add the ability for users to create and account and login. Direct the user to the home page after login.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,15 +8,13 @@ const NavBar = () => {
         <Container>
             <div>
                 <Navbar className={styles.NavBar} bg="dark" expand='md' fixed="top" variant="dark">
-                    <NavLink to="/">
-                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    </NavLink>
+                    <Navbar.Brand as={NavLink} to="/">Navbar</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/"><i className="fas fa-home"></i>Home</NavLink>
-                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i className="fas fa-sign-in-alt"></i>Sign In</NavLink>
-                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/register"><i className="fas fa-user-plus"></i>Register</NavLink>
+                            <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/"><i className="fas fa-home"></i>Home</Nav.Link>
+                            <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
+                            <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/register"><i className="fas fa-user-plus"></i>Register</Nav.Link>
                         </Nav>
                         <Form inline>
                             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -27,6 +25,6 @@ const NavBar = () => {
             </div>
         </Container>
     );
-    }
+}
 
 export default NavBar;
--- src/pages/auth/RegistrationForm.js
+++ src/pages/auth/RegistrationForm.js
@@ -1,40 +1,79 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
 
 const Registration = () => {
+
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+
+  const { username, password1, password2 } = signUpData;
+
+  const [error, setError] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("https://kinnect-api-cf0f665319fa.herokuapp.com/dj-rest-auth/registration/", signUpData)
+      history.push("/signin")
+    } catch(err) {
+      console.error("Error response:", err.response?.data);
+      setError(err.response?.data)
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" value={username} onChange={handleChange} />
             </Form.Group>
+            {error.username?.map((message, idx) =>
+            <Alert variant="warning" key={idx}> {message} </Alert>)}
 
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1" />
+              <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1" value={password1} onChange={handleChange} />
             </Form.Group>
+            {error.password1?.map((message, idx) =>
+            <Alert variant="warning" key={idx}> {message} </Alert>)}
+
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm Password</Form.Label>
-              <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="password2" />
+              <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={handleChange} />
             </Form.Group>
+            {error.password2?.map((message, idx) =>
+            <Alert variant="warning" key={idx}> {message} </Alert>)}
 
             <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
               Register
             </Button>
+            {error.non_field_errors?.map((message, idx) =>
+            <Alert variant="warning" className="mt-3" key={idx}> {message} </Alert>)}
+
           </Form> 
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
@@ -56,6 +95,6 @@ const Registration = () => {
       </Col>
     </Row>
   );
-};
+}
 
-export default Registration;
+export default Registration
