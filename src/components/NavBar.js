577b620b6d1ff7f405c1c1f9cd23fe64f2a4544a
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-23 23:09:46 +0000

    Add feed, liked, sign out and profile to the navbar. Create a js file to handle avatars so they render correctly on each page.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,24 @@ import styles from '../styles/NavBar.module.css';
 import { Navbar, Nav, Container, Form, FormControl, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
 
-    const loggedInIcons = <>{currentUser?.username}</>
-    const loggedOutIcons = (
+    const addPostIcon = (
+        <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/posts/create"><i className="fas fa-plus"></i>Add Post</Nav.Link>
+    )
+    const loggedInIcons = (
+        <>
+            <Nav.Link className={styles.NavLink} activeClassName={styles.Active} to="/feed"><i className="fas fa-stream"></i>Feed</Nav.Link>
+            <Nav.Link className={styles.NavLink} activeClassName={styles.Active} to="/liked"><i className="fas fa-heart"></i>Liked</Nav.Link>
+            <Nav.Link className={styles.NavLink} to="/" onClick={() => {}} ><i className="fas fa-sign-out-alt"></i>Sign Out</Nav.Link>
+            <Nav.Link className={styles.NavLink} to={`/profiles/${currentUser?.profile_id}`}><Avatar src={currentUser?.profile_image} text="Profile" height={40} /> </Nav.Link>
+        </>
+    );
+    
+        const loggedOutIcons = (
         <>
             <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
             <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/register"><i className="fas fa-user-plus"></i>Register</Nav.Link>
@@ -20,6 +32,7 @@ const NavBar = () => {
             <div>
                 <Navbar className={styles.NavBar} bg="dark" expand='md' fixed="top" variant="dark">
                     <Navbar.Brand as={NavLink} to="/">Navbar</Navbar.Brand>
+                    {currentUser && addPostIcon}
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
