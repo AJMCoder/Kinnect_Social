42e45e2b4ccaefba51fda4195f1bc36b2457d42d
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-23 23:26:58 +0000

    Create a handlesignout function. Update the navbar so that it identifies if the user is logged in and changes their view depending on if they are or not.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,53 +1,79 @@
 import React from "react";
-import styles from '../styles/NavBar.module.css';
 import { Navbar, Nav, Container, Form, FormControl, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
+import axios from "axios";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post("https://kinnect-api-cf0f665319fa.herokuapp.com/dj-rest-auth/logout/");
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const addPostIcon = (
-        <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/posts/create"><i className="fas fa-plus"></i>Add Post</Nav.Link>
-    )
+        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
+            <i className="fas fa-plus"></i>Add Post
+        </NavLink>
+    );
+
     const loggedInIcons = (
         <>
-            <Nav.Link className={styles.NavLink} activeClassName={styles.Active} to="/feed"><i className="fas fa-stream"></i>Feed</Nav.Link>
-            <Nav.Link className={styles.NavLink} activeClassName={styles.Active} to="/liked"><i className="fas fa-heart"></i>Liked</Nav.Link>
-            <Nav.Link className={styles.NavLink} to="/" onClick={() => {}} ><i className="fas fa-sign-out-alt"></i>Sign Out</Nav.Link>
-            <Nav.Link className={styles.NavLink} to={`/profiles/${currentUser?.profile_id}`}><Avatar src={currentUser?.profile_image} text="Profile" height={40} /> </Nav.Link>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/feed">
+                <i className="fas fa-stream"></i>Feed
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/liked">
+                <i className="fas fa-heart"></i>Liked
+            </NavLink>
+            <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+                <i className="fas fa-sign-out-alt"></i>Sign Out
+            </NavLink>
+            <NavLink className={styles.NavLink} to={`/profiles/${currentUser?.profile_id}`}>
+                <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+            </NavLink>
         </>
     );
-    
-        const loggedOutIcons = (
+
+    const loggedOutIcons = (
         <>
-            <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
-            <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/register"><i className="fas fa-user-plus"></i>Register</Nav.Link>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+                <i className="fas fa-sign-in-alt"></i>Sign In
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/register">
+                <i className="fas fa-user-plus"></i>Register
+            </NavLink>
         </>
     );
 
     return (
         <Container>
-            <div>
-                <Navbar className={styles.NavBar} bg="dark" expand='md' fixed="top" variant="dark">
-                    <Navbar.Brand as={NavLink} to="/">Navbar</Navbar.Brand>
-                    {currentUser && addPostIcon}
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link as={NavLink} className={styles.NavLink} activeClassName={styles.Active} to="/"><i className="fas fa-home"></i>Home</Nav.Link>
-                            {currentUser ? loggedInIcons : loggedOutIcons}
-                        </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-info">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
+            <Navbar className={styles.NavBar} bg="dark" expand="md" fixed="top" variant="dark">
+                <Navbar.Brand as={NavLink} to="/">Navbar</Navbar.Brand>
+                {currentUser && addPostIcon}
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/">
+                            <i className="fas fa-home"></i>Home
+                        </NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-info">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
         </Container>
     );
-}
+};
 
 export default NavBar;
