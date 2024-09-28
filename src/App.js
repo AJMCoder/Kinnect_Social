9d1453a5bd1c34ffb8ff9da0bdb06b6f68e41623
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-28 23:02:37 +0000

    Create a profilePage that displays followers, posts and following count for its users.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import PostEditForm from "./pages/posts/PostEditForm";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 
 function App() {
@@ -32,6 +33,7 @@ function App() {
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
               <Route exact path="/posts/:id" render={() => <PostPage />} />
               <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+              <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
               <Route render={() => <h1>404 Not Found</h1>} />
             </Switch>
           </Container>
