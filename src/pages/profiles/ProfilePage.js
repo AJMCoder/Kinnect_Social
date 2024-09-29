ae7accdcb85a7cc3cb89dfd46bae36f9320aa2cf
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-29 19:29:29 +0000

    Create the function allowing users to unfollow accounts they had previouslly followed, updating the count to represent this change to both profiles.

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -28,7 +28,7 @@ function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
   const { id } = useParams();
-  const {setProfileData, handleFollow} = useSetProfileData();
+  const {setProfileData, handleFollow, handleUnfollow} = useSetProfileData();
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
@@ -81,7 +81,7 @@ function ProfilePage() {
         <Col lg={3} className="text-lg-right">
           {currentUser && !is_owner && (
             profile?.following_id ? (
-              <Button className={`${btnStyles.Button} ${btnStyles.BlackOutline}`} onClick={() => {}}>Unfollow</Button>
+              <Button className={`${btnStyles.Button} ${btnStyles.BlackOutline}`} onClick={() => handleUnfollow(profile)}>Unfollow</Button>
             ) : (
               <Button className={`${btnStyles.Button} ${btnStyles.Black}`} onClick={() => handleFollow(profile)}>Follow</Button>
             )
